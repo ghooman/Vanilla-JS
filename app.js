1a070d6819293c9a52c14be1cf3c66a9be63c260
ghooman <ghoopark1@gmail.com>
2022-10-04 00:19:20 +0900

    login form practice

--- app.js
+++ app.js
@@ -6,3 +6,26 @@
 $(".navbar-toggler").on("click", function () {
   $(".list-group").toggleClass("show");
 });
+
+$("#login").on("click", function () {
+  $(".black-bg").addClass("show-modal");
+});
+
+$("#close").on("click", function () {
+  $(".black-bg").removeClass("show-modal");
+});
+
+$("form").on("submit", function (e) {
+  if (document.getElementById("email").value === "") {
+    e.preventDefault();
+    alert("아이디를 입력하세요");
+  }
+  if (document.getElementById("pw").value === "") {
+    e.preventDefault();
+    alert("비밀번호를 입력하세요");
+  }
+  if (document.getElementById("pw").value.length < 6) {
+    e.preventDefault();
+    alert("비밀번호를 6자 이상 입력하세요.");
+  }
+});
